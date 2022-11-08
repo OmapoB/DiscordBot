015a530390df04ea4083f07bc18d8978520c14a6
Gasan <xlampogialo@gmail.com>
2022-11-09 00:17:57 +0300

    Изменил обработчик команд
    Все для соответствия новым прицнипам

--- src/main/java/ru/omarov/commands/CommandsHandler.java
+++ src/main/java/ru/omarov/commands/CommandsHandler.java
@@ -3,19 +3,16 @@ package ru.omarov.commands;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import ru.omarov.commands.list.HelpCommand;
+import ru.omarov.commands.list.PlayCommand;
 
 public class CommandsHandler extends ListenerAdapter {
-    private String reply = "";
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-        if (event.getName().equals(CommandsEnum.HELP.getName())) {
-            for (CommandsEnum command: CommandsEnum.values()) {
-                reply = reply.concat(command.getName() + ": " + command.getDescription() + "\n");
-            }
-            event.reply(reply).queue();
-        } else if (event.getName().equals(CommandsEnum.PLAY.getName())) {
-            reply = reply.concat("сейчас играет: " + "ничего");
-            event.reply(reply).queue();
+        if (event.getName().equals(HelpCommand.allCommands.get("help").getText())) {
+            event.reply(HelpCommand.getHelp()).queue();
+        } else if (event.getName().equals(HelpCommand.allCommands.get("play").getText())) {
+            event.reply(PlayCommand.play()).queue();
         }
     }
 }
