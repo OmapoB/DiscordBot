8adcf95fed97f708071d637a5dbe8ff245debbe6
Gasan <xlampogialo@gmail.com>
2022-12-01 22:12:00 +0300

    Добавил команду NamesCommand
    Команда выводит список доступных никнеймов

--- src/main/java/ru/omarov/commands/CommandsHandler.java
+++ src/main/java/ru/omarov/commands/CommandsHandler.java
@@ -3,9 +3,7 @@ package ru.omarov.commands;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
-import ru.omarov.commands.list.HelpCommand;
-import ru.omarov.commands.list.KickCommand;
-import ru.omarov.commands.list.PlayCommand;
+import ru.omarov.commands.list.*;
 
 import java.util.Map;
 
@@ -13,13 +11,14 @@ public class CommandsHandler extends ListenerAdapter {
     public static final Map<String, Command> COMMANDS = Map.ofEntries(
             Map.entry("help", new HelpCommand()),
             Map.entry("play", new PlayCommand()),
-            Map.entry("kick", new KickCommand())
+            Map.entry("kick", new KickCommand()),
+            Map.entry("names", new NamesCommand())
     );
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         if (event.getUser().isBot()) {
-            event.reply("Бот, пошел на йух!");
+            event.reply("Бот, пошел на йух!").queue();
             return;
         }
         COMMANDS.get(event.getName()).execute(event);
--- src/main/java/ru/omarov/commands/list/NamesCommand.java
+++ src/main/java/ru/omarov/commands/list/NamesCommand.java
@@ -1,4 +1,56 @@
 package ru.omarov.commands.list;
 
-public class NamesCommand {
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
+import org.jetbrains.annotations.NotNull;
+import ru.omarov.commands.AbstractCommand;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NamesCommand extends AbstractCommand {
+    private List<SelectOption> namesList = new ArrayList<>();
+
+    public NamesCommand() {
+        this.setName("names");
+        this.setDescription("Выводит список доступных никнеймов");
+        this.addNames(List.of(
+                "Конев",
+                "Жуков",
+                "Малиновский",
+                "Берия",
+                "Космодемьянская",
+                "Савицкая"
+        ));
+    }
+
+    @Override
+    public void execute(@NotNull SlashCommandInteractionEvent event) {
+        StringSelectMenu list = StringSelectMenu.create("select:names")
+                .setPlaceholder("Раскрой")
+                .setRequiredRange(1, 1)
+                .addOptions(namesList)
+                .build();
+
+        event.reply("Выберите себе никнейм")
+                .setEphemeral(true)
+                .addActionRow(list)
+                .queue();
+    }
+
+    public List<SelectOption> getNamesList() {
+        return namesList;
+    }
+
+    public void addName(String name) {
+        namesList.add(SelectOption.of(name, "ник"));
+    }
+
+    public void addNames(List<String> names) {
+        for (String name :
+                names) {
+            namesList.add(SelectOption.of(name, name));
+        }
+    }
 }
